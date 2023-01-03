9f7dbbdfd2ee6e55dad666027502208e84374765
jose-robles2 <92705704+jose-robles2@users.noreply.github.com>
2023-01-02 18:56:26 -0800

    Add files via upload

---
+++ app/src/Libraries.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdlib.h>
+#include <iostream>
+using namespace std; 
+
+#include <string> 
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <stdexcept> // std::runtime_error
+#include <sstream>
+#include <iostream>
